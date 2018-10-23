4cd4b41b9f4884cff1957f94e8f55294bec57622
kshitij kumar <kshitijk83@gmail.com>
2018-10-23 23:04:30 +0530

    adding methods

--- wisher.js
+++ wisher.js
@@ -4,7 +4,80 @@
         return new Greeter.init(firstname, lastname, language); // this will return an object which will have __proto__ as greeter.init.prototype
     }
 
-    Greeter.prototype = {};
+    var supportedLangs = ["en", "es"];
+
+    var greetings = {
+        en: "Hello",
+        es: "Hola"
+    };
+
+    var formalGreetings = {
+        en: "Greetings",
+        es: "Saludos"
+    };
+
+    var logMessages = {
+        en: "Logged in",
+        es: "inició sesión"
+    } // these variable are created bcoz they cant be accessed by outside source
+
+    Greeter.prototype = {
+        
+        fullname: function() {
+            return this.firstname+" "+this.lastname;
+        },
+
+        validate: function() {
+            if(supportedLangs.indexOf(this.language) === -1) {
+                throw "Invalid language";
+            }
+        },
+
+        greeting: function() {
+            return greetings[this.language]+" "+this.firstname+"!";
+        },
+
+        formalGreetings: function() {
+            return formalGreetings[this.language]+", "+this.fullname();
+        },
+
+        greet: function(formal) {
+            var msg;
+
+            // if defined or null it will be coerced to false
+            if(formal) {
+                msg = this.formalGreetings();
+            }
+            else {
+                msg = this.greeting();
+            }
+
+            if(console) {
+                console.log(msg);
+            }
+
+            // "this" refers to the calling object at execution time
+            // makes the method chainable
+            return this;
+        },
+
+        log: function() {
+            if(console) {
+                console.log(logMessages[this.language]+": "+this.fullname());
+            }
+
+            return this;
+        },
+
+        setLang: function(lang) {
+            this.language = lang;
+            this.validate();
+
+            return this;
+        },
+
+        
+    };
 
     Greeter.init = function(firstname, lastname, language) {
         var self = this; // saving this in a var so that i dont have any problem in near future
